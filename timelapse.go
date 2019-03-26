504bd6549698ebb2e545668d0240da80efb06ce0
David McQueen <david@dmcqueen.co.uk>
2019-03-26 18:34:24 +0000

    Couple of Comments

--- timelapse.go
+++ timelapse.go
@@ -15,7 +15,7 @@ func main(){
 	var (
 		dir = flag.String("dir", "./images", "Directory to find the images in")
 		fps = flag.Uint("fps", 10, "Framerate")
-		outputFile = flag.String("outputFile", "timelapse.avi", "Path and name of output file.")
+		outputFile = flag.String("outputFile", "timelapse.avi", "Name of output file.")
 		limit = flag.Uint("limit", 3000, "Maximum number of images to stitch")
 	)
 	flag.Parse()
@@ -33,6 +33,7 @@ func stitchImages(dir string, fps uint, outputFile string, limit uint) {
 
 	fmt.Println("Found images: ", len(files))
 
+	// Can't process more images than we have
 	if limit > uint(len(files)) {
 		limit = uint(len(files))
 	}
@@ -45,8 +46,8 @@ func stitchImages(dir string, fps uint, outputFile string, limit uint) {
 
 	newImage = gocv.IMRead(firstImage, gocv.IMReadAnyColor)
 
+	// Use the dimensions of the first image for the dimensions of all images
 	imageWidth, imageHeight := newImage.Cols(), newImage.Rows()
-
 
 	writer, err := gocv.VideoWriterFile(outputFile, "MJPG", float64(fps), newImage.Cols(), newImage.Rows(), true)
 
@@ -57,16 +58,19 @@ func stitchImages(dir string, fps uint, outputFile string, limit uint) {
 	defer writer.Close()
 
 	fmt.Println("Video being generated with the name :", outputFile)
-	fmt.Println("Processinf files upto limit: ", limit)
+	fmt.Println("Processing files upto limit: ", limit)
 
 	for _, image := range files[:limit] {
 		processImage(writer, image, dir, imageWidth, imageHeight)
 	}
 }
 
+///
+/// Take an image, resize it if necessary, and add it to the writer
+///
 func processImage(writer *gocv.VideoWriter, image os.FileInfo, dir string, imageWidth int, imageHeight int) {
 	if image.IsDir() || !strings.HasSuffix(image.Name(), "jpg") {
-		return
+		return // Nothing to do...
 	}
 
 	newImage := gocv.IMRead(dir + "/" + image.Name(), gocv.IMReadAnyColor)
@@ -75,14 +79,12 @@ func processImage(writer *gocv.VideoWriter, image os.FileInfo, dir string, image
 	resizedImage := gocv.NewMat()
 	defer resizedImage.Close()
 
+	// Specify the size that the image needs to be for it to be added to the output file
 	var pointy image2.Point
-
 	pointy.X = imageWidth
 	pointy.Y = imageHeight
 
-
 	gocv.Resize(newImage, &resizedImage, pointy, 0, 0, gocv.InterpolationLinear)
-
 
 	err := writer.Write(resizedImage)
 
